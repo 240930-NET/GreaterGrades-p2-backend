4ba5af603950e155760e5a34ab89d05307b345b0
RylanMattocks <rmattoc@wgu.edu>
2024-11-02 12:41:56 -0400

    having issues with app.js, previously most likely working bc of saved auth tokens, temp fix added but needs adjusted

--- greatergradesfrontend/src/App.js
+++ greatergradesfrontend/src/App.js
@@ -1,6 +1,8 @@
+/*
+
 import './App.css';
 import './styles/student.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import AppRoutes from './routes/AppRoutes';
 
@@ -12,40 +14,84 @@ function App() {
 
 
   useEffect(() => {
-      const fetchTokenAndUser = async () => {
-        try {
-          if (!token){
-            const tokenResponse = await fetch('http://localhost:5000/api/Auth/login', {
-              method: 'POST',
-              headers: {'Content-Type': 'application/json'},
-              body: JSON.stringify({
-                  username: "admin",
-                  password: "admin"
-              })
-            });
-            const tokenData = await tokenResponse.json();
-            setToken(tokenData);
-            localStorage.setItem('authToken', tokenData?.token)
-          }
+    const fetchToken = async () => {
+      if (!token) {
+        const response = await fetch('http://localhost:5000/api/Auth/login', {
+          method: 'POST',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify({
+              username: "admin",
+              password: "admin"
+          })
+        });
+        const data = await response.json();
+        setToken(data);
+        localStorage.setItem('authToken', data?.token)
+      }
+    };
 
-          const userResponse = await fetch('http://localhost:5000/api/Auth/2', {
-            headers: {
-              'Authorization': `Bearer ${localStorage.getItem('authToken')}`,
-              'Content-Type': 'application/json'
-            }
-          });
-          const userData = await userResponse.json();
-          setUser(userData);
-          localStorage.setItem('currentUser', JSON.stringify(userData));
-          
-        } catch {
-
-        } finally {
-          setLoading(false);
+    const fetchUser = async () => {
+      const response = await fetch('http://localhost:5000/api/Auth/2', {
+        headers: {
+          'Authorization': `Bearer ${localStorage.getItem('authToken')}`,
+          'Content-Type': 'application/json'
         }
-      };
-      fetchTokenAndUser();
-  }, [token]);
+      });
+      const data = await response.json();
+      setUser(data);
+      localStorage.setItem('currentUser', JSON.stringify(data));
+    };
+
+    const initialize = async () => {
+      await fetchToken();
+      if (localStorage.getItem('authToken')) {
+        await fetchUser();
+      }
+      setLoading(false)
+    };
+
+    initialize();
+  }, []);
+
+
+
+
+
+  // useEffect(() => {
+  //     const fetchTokenAndUser = async () => {
+  //       try {
+  //         if (!token){
+  //           const tokenResponse = await fetch('http://localhost:5000/api/Auth/login', {
+  //             method: 'POST',
+  //             headers: {'Content-Type': 'application/json'},
+  //             body: JSON.stringify({
+  //                 username: "admin",
+  //                 password: "admin"
+  //             })
+  //           });
+  //           const tokenData = await tokenResponse.json();
+  //           setToken(tokenData);
+  //           localStorage.setItem('authToken', tokenData?.token)
+  //         }
+
+  //         const userResponse = await fetch('http://localhost:5000/api/Auth/2', {
+  //           headers: {
+  //             'Authorization': `Bearer ${localStorage.getItem('authToken')}`,
+  //             'Content-Type': 'application/json'
+  //           }
+  //         });
+  //         const userData = await userResponse.json();
+  //         setUser(userData);
+  //         localStorage.setItem('currentUser', JSON.stringify(userData));
+          
+  //       } catch {
+
+  //       } finally {
+  //         setLoading(false);
+  //       }
+  //     };
+  //     fetchTokenAndUser();
+  // }, [token]);
 
   if (loading) {
     return <div>loading....</div>
@@ -60,3 +106,102 @@ function App() {
 }
 
 export default App;
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+import './App.css';
+import './styles/student.css';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import AppRoutes from './routes/AppRoutes';
+
+function App() {
+
+  //// Fetch admin login and set authentication token
+  const [token, setToken] = useState("");
+    useEffect(() => {
+        const fetchToken = async () => {
+            const response = await fetch('http://localhost:5000/api/Auth/login', {
+                method: 'POST',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify({
+                    username: "admin",
+                    password: "admin"
+                })
+            });
+            const data = await response.json();
+            setToken(data);
+        }
+        fetchToken();
+    }, []);
+    
+
+    useEffect(() => {
+      if (token) {
+        localStorage.setItem('authToken', token?.token);
+      }
+    }, [token]);
+
+    ///// Fetch admin by id (2) and set role
+    const [user, setUser] = useState({});
+    useEffect(() => {
+        const fetchUser = async () => {
+          try{
+          const response = await fetch('http://localhost:5000/api/Auth/2', {
+            headers: {
+              'Authorization': `Bearer ${localStorage.getItem('authToken')}`,
+              'Content-Type': 'application/json'
+            }
+          })
+          const data = await response.json();
+          setUser(data);
+          } catch{
+          }
+        }
+      fetchUser();
+    }, [token]);
+
+    useEffect(() => {
+      if (user) {
+        localStorage.setItem('currentUser', JSON.stringify(user));
+      }
+    }, [user]);
+
+
+
+
+  return (
+    <Router>
+      <AppRoutes />
+    </Router>
+  )
+}
+
+export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
