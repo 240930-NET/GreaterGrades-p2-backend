da54a566a34953224d13804fefaadf450d1b0d40
RylanMattocks <rmattoc@wgu.edu>
2024-11-01 20:17:57 -0400

    added info to enrolled classes and taught classes, all sidebars should display proper data if user is admin

--- greatergradesfrontend/src/components/EnrolledClasses.js
+++ greatergradesfrontend/src/components/EnrolledClasses.js
@@ -1,7 +1,18 @@
+import Tiles from "./Tiles";
 const EnrolledClasses = () => {
+
+    const storedUser = localStorage.getItem('currentUser');
+    const parsedUser = JSON.parse(storedUser);
+
+
     return (
         <div className="student-content">
-            <p>Testing enrolled.....</p>
+            <div>
+                <h3>Enrolled Classes</h3>
+                <div className="student-tiles">
+                    <Tiles courseIds={parsedUser?.classIds} />
+                </div>
+            </div>
         </div>
     )
 }
--- greatergradesfrontend/src/components/TaughtClasses.js
+++ greatergradesfrontend/src/components/TaughtClasses.js
@@ -1,7 +1,18 @@
+import Tiles from "./Tiles";
+
 const TaughtClasses = () => {
+    const storedUser = localStorage.getItem('currentUser');
+    const parsedUser = JSON.parse(storedUser);
+
+
     return (
         <div className="student-content">
-            <p>Testing taught.....</p>
+            <div>
+                <h3>Taught Classes</h3>
+                <div className="student-tiles">
+                    <Tiles courseIds={parsedUser?.taughtClassIds} />
+                </div>
+            </div>
         </div>
     )
 }
