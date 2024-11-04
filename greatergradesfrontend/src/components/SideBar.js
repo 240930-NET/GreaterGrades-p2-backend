c76f5196df308f59e5a8f32f9e2df8fe3fdd4074
Purplebelt1 <wdeb2013@gmail.com>
2024-11-04 03:16:14 -0600

    added enrolled classes dropdown

--- greatergradesfrontend/src/components/SideBar.js
+++ greatergradesfrontend/src/components/SideBar.js
@@ -1,15 +1,34 @@
-const SideBar = ({items, selectedItem, onSelectItem}) => {
+import { useState } from 'react';
+
+const SideBar = ({ items, selectedItem, onSelectItem }) => {
+    const [dropdownOpen, setDropdownOpen] = useState(false);
+
+    const handleItemClick = (item) => {
+        onSelectItem(item.id);
+        if (item.id === 'enrolled classes') {
+            setDropdownOpen(!dropdownOpen);
+        } else {
+            setDropdownOpen(false);
+        }
+    };
 
     return (
         <div className="dashboard-sidebar">
             <ul>
                 {items.map(item => (
-                    <li key={item.id} className={`${item.id === selectedItem ? 'dashboard-sidebar-li-selected' : ''}`} onClick={() => onSelectItem(item.id)}>{item.label}
-                        <ul>
-                            {item.assignmentLabels?.map(assignment => (
-                                <li key={assignment}>{assignment}</li>
-                            ))}
-                        </ul>
+                    <li 
+                        key={item.id} 
+                        className={`${item.id === selectedItem ? 'dashboard-sidebar-li-selected' : ''}`} 
+                        onClick={() => handleItemClick(item)}
+                    >
+                        {item.label}
+                        {item.id === 'enrolled classes' && dropdownOpen && (
+                            <ul>
+                                {item.assignmentLabels?.map(assignment => (
+                                    <li key={assignment}>{assignment}</li>
+                                ))}
+                            </ul>
+                        )}
                     </li>
                 ))}
             </ul>
