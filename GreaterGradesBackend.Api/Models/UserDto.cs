91a674b11831cc5612925772e6a7d0880e939bbf
Purplebelt1 <wdeb2013@gmail.com>
2024-11-07 08:28:54 -0600

    Typo

--- GreaterGradesBackend.Api/Models/UserDto.cs
+++ GreaterGradesBackend.Api/Models/UserDto.cs
@@ -11,7 +11,7 @@ namespace GreaterGradesBackend.Api.Models
         public string LastName { get; set; }
         public ICollection<int> ClassIds { get; set; }
         public ICollection<int> TaughtClassIds { get; set; }
-        public ICollection<int> GradesIds { get; set; }
+        public ICollection<int> GradeIds { get; set; }
         public Role Role { get; set; }
         public int InstitutionId { get; set; }
     }
